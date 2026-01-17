f0c5c11eee752e0b0f2ea181cd17afd9d610041a
iamplayboicarti <sieutuanvip@gmail.com>
2026-01-17 09:32:21 +0700

    Spring_LaptopShop_Ecommerce

---
+++ src/main/java/com/example/laptopshop/LaptopshopApplication.java
@@ -0,0 +1,18 @@
+package com.example.laptopshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class LaptopshopApplication {
+
+	public static void main(String[] args) {
+	ApplicationContext DaoAnhTuan = SpringApplication.run(LaptopshopApplication.class, args);
+		for(String s: DaoAnhTuan.getBeanDefinitionNames())
+		{
+		System.out.println(s);	
+		}
+	}
+
+}
---
+++ src/main/java/com/example/laptopshop/services/MyService.java
@@ -0,0 +1,7 @@
+package com.example.laptopshop.services;
+
+import org.springframework.stereotype.Service;
+@Service
+public class MyService {
+    
+}
