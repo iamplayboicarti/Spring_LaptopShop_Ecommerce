3e371183a414500c67a8fdf3fd33e9cd2d03c20b
iamplayboicarti <sieutuanvip@gmail.com>
2026-01-30 00:46:03 +0700

    Layout

---
+++ src/main/java/com/example/laptopshop/controller/admin/DashboardController.java
@@ -0,0 +1,15 @@
+package com.example.laptopshop.controller.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+public class DashboardController {
+    @GetMapping("/admin")
+    public String getDashBoard() {
+        return "admin/dashboard/show";
+    }
+    
+}
---
+++ src/main/java/com/example/laptopshop/controller/admin/OrderController.java
@@ -0,0 +1,15 @@
+package com.example.laptopshop.controller.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+public class OrderController {
+    @GetMapping("/admin/order")
+    public String getDashBoard() {
+        return "admin/order/show";
+    }
+    
+}
